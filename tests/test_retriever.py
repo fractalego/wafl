5c5292548ebbcd547467f1f4a229293488bdb7f9
Fractalego <albertocetoli@gmail.com>
2021-12-15 12:20:46 +0000

    added retriever

---
+++ tests/test_retriever.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from wafl.retriever import Retriever
+
+
+class TestParsing(TestCase):
+
+    def test_retrieval(self):
+        retriever = Retriever()
+        sentences = ['this is a test', 'the food is hot on the table']
+        for index, sentence in enumerate(sentences):
+            retriever.add_text_and_index(sentence, index)
+
+        query = 'the food is warm'
+        expected = 1
+        predicted = retriever.get_indices_and_scores_from_text(query)
+        assert predicted[0][0] == expected
--- wafl/retriever.py
+++ wafl/retriever.py
@@ -1,10 +1,8 @@
-import json
 import os
 import torch
 import logging
 
 from gensim.models import KeyedVectors
-from tqdm import tqdm
 from sentence_transformers import SentenceTransformer
 
 _path = os.path.dirname(__file__)
@@ -15,51 +13,17 @@ sentence_model = SentenceTransformer('msmarco-distilbert-base-v3')
 sentence_model = sentence_model.to(device)
 
 
-def get_embeddings_from_text(text):
-    outputs = sentence_model.encode(text)
-    return outputs
+class Retriever:
+    _embeddings_model = KeyedVectors(768)
+
+    def add_text_and_index(self, text, index):
+        embeddings = _get_embeddings_from_text(text)
+        self._embeddings_model.add_vector(index, embeddings)
+
+    def get_indices_and_scores_from_text(self, text):
+        embeddings = _get_embeddings_from_text(text)
+        return self._embeddings_model.similar_by_vector(embeddings, topn=2)
 
 
-def get_embeddings_from_dialogue_and_query(dialogue, query):
-    dialogue_embeddings = get_embeddings_from_text(dialogue)
-    query_embeddings = get_embeddings_from_text(query)
-    return (query_embeddings + 0.01 * dialogue_embeddings) / 1.01
-
-
-def get_documents_and_scores(dialogue, query):
-    try:
-        chapter_embeddings = KeyedVectors.load(os.path.join(_path, '../cache/embeddings.gensim'))
-    except EOFError:
-        raise RuntimeError("No file has been loaded")
-    embeddings = get_embeddings_from_dialogue_and_query(dialogue, query)
-    results = chapter_embeddings.similar_by_vector(embeddings, topn=2)
-    return results
-
-
-def update_chapters_embeddings():
-    chapter_embeddings = KeyedVectors(768)
-
-    file_paths = json.load(open(os.path.join(_path, '../cache/files.json')))
-    for filename in file_paths:
-        _logger.info(f'Reading {filename}.')
-        text = open(filename, encoding='utf-8').read()
-        chapters = get_chapters_from_text(text)
-        for chapter in tqdm(chapters):
-            if len(chapter.split()) < 10:
-                continue
-            chapter = ' '.join(chapter.split()[:256])
-            embeddings = get_embeddings_from_text(chapter)
-            chapter_embeddings.add_vector(chapter, embeddings)
-        _logger.info(f'Finished reading {filename}.')
-
-    chapter_embeddings.save(os.path.join(_path, '../cache/embeddings.gensim'))
-
-
-def reset_embeddings():
-    chapter_embeddings = KeyedVectors(768)
-    chapter_embeddings.save(os.path.join(_path, '../cache/embeddings.gensim'))
-
-
-def reset_files():
-    with open(os.path.join(_path, '../cache/files.json'), 'w') as file:
-        file.write('[]')
+def _get_embeddings_from_text(text):
+    return sentence_model.encode(text)
