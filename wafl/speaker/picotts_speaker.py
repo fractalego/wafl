358694b36e6b1b27a2644981cc257b910aef5697
Fractalego <albertocetoli@gmail.com>
2021-12-27 19:20:24 +0000

    Saving memory by intiliasing pycotts for every vocalization

--- wafl/speaker/picotts_speaker.py
+++ wafl/speaker/picotts_speaker.py
@@ -1,18 +1,21 @@
-from time import sleep
-
 import pyaudio
 from picotts import PicoTTS
 
 
 class PiCoTTSSpeaker:
     def __init__(self, voice="en-GB"):
-        self._picotts = PicoTTS()
-        self._picotts.voice = voice
         self._pyaudio = pyaudio.PyAudio()
-        self._stream = self._pyaudio.open(
-            format=pyaudio.paInt16, channels=1, rate=16000, input=False, output=True
-        )
+        self._voice = voice
 
     def speak(self, text):
-        waveform = self._picotts.synth_wav(text)
-        self._stream.write(waveform[100:])
+        picotts = PicoTTS()
+        picotts.voice = self._voice
+        waveform = picotts.synth_wav(text)
+        stream = self._pyaudio.open(
+            format=pyaudio.paInt16, channels=1, rate=16000, input=False, output=True
+        )
+        stream.write(waveform[100:])
+        stream.stop_stream()
+        stream.close()
+        del stream
+        del picotts
