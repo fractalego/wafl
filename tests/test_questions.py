1c3835577412ecada73041610664d1bfb3e8fbab
Fractalego <albertocetoli@gmail.com>
2022-01-04 16:18:24 +0000

    Added tests for important questions to get right

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from wafl.conversation.utils import is_question
+from wafl.qa.qa import QA, Query
 
 
 class TestQuestions(TestCase):
@@ -12,6 +13,13 @@ class TestQuestions(TestCase):
         utterance = "Am I interested in this platter of fish"
         assert is_question(utterance)
 
-    def test_am_question(self):
-        utterance = "Am I interested in this platter of fish"
-        assert is_question(utterance)
+    def test_yes_qa(self):
+        query = Query(text="Is the user satisfied with this", is_question=True)
+        user_answer = (
+            "When asked 'is the user satisfied with this', the user says: 'yes I am.'"
+        )
+
+        qa = QA()
+        answer = qa.ask(query, user_answer)
+
+        assert answer.text == "yes"
