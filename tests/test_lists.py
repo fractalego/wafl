53e3f63e9e17fe507ce6aee2163a6360fc1685a1
Fractalego <albertocetoli@gmail.com>
2022-02-06 11:10:44 +0000

    Added list tests

---
+++ tests/test_lists.py
@@ -0,0 +1,83 @@
+import os
+import wave
+from unittest import TestCase
+
+import numpy as np
+
+from wafl.conversation.conversation import Conversation
+from wafl.inference.backward_inference import BackwardInference
+from wafl.interface.dummy_interface import DummyInterface
+from wafl.knowledge.knowledge import Knowledge
+from wafl.listener.wav2vec2_listener import Wav2Vec2Listener
+from wafl.qa.qa import Query
+
+_path = os.path.dirname(__file__)
+
+_rules = """
+
+item = what does the user want to add to the shopping list?
+  add_shopping_list(item)
+  SAY {item} has been added to the list
+  ! _ask_another_item
+
+item = what does the user want to add to the shopping list?
+  add_shopping_list(item)
+  !{item} can be part of a shopping list
+  SAY Please speak more clearly
+
+item = what does the user want to remove from the shopping list?
+  remove_from_shopping_list(item)
+  items = get_shopping_list_in_english()
+
+_ask_another_item
+  does the user want to add another item
+  item = what do you want to add to the shopping list
+  add_shopping_list(item)
+  SAY {item} has been added to the list
+  _ask_another_item
+
+_ask_another_item
+  does the user want to add another item
+  item = what do you want to add to the shopping list
+  !{item} can be part of a shopping list
+  SAY {item} cannot be part of a shopping list
+  _ask_another_item
+
+"remove an item from the shopping list"
+  item = What do you want to remove?
+  remove_from_shopping_list(item)
+  items = get_shopping_list_in_english()
+
+the user wants to delete the shopping list
+  Do you want to delete the current shopping list
+  reset_shopping_list()
+  SAY The shopping list has been deleted
+
+the user wants to know what is in the shopping list
+  items = get_shopping_list_in_english()
+  SAY The shopping list contains: {items}
+
+"What should I buy"
+  the user wants to know what is in the shopping list
+
+"""
+
+
+class TestNew(TestCase):
+    def test_second_rule_is_not_run_if_prior_clause_fails(self):
+        interface = DummyInterface(
+            [
+                "add apples to the shopping list",
+                "no",
+                "remove apples from the shopping list",
+                "no",
+            ]
+        )
+        conversation = Conversation(
+            Knowledge(_rules), interface=interface, code_path="functions"
+        )
+        conversation.input()
+        conversation.input()
+        output = "\n".join(interface.utterances)
+        print(interface.utterances)
+        assert output.count("Do you want to remove apples from the shopping list") == 1
