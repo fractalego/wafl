d5c77df8217d6dbb6abc72c84f933081de4711ca
Fractalego <albertocetoli@gmail.com>
2022-10-16 17:34:35 +0100

    Version 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="wafl",
-    version="0.0.3",
+    version="0.0.4",
     url="http://github.com/fractalego/wafl",
     author="Alberto Cetoli",
     author_email="alberto@fractalego.io",
