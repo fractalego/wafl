d96d6f81dfe6ceaea4c8d46cd39e59fc8f3fcee6
Fractalego <albertocetoli@gmail.com>
2022-12-28 19:26:11 +0000

    saving memory by using a dictionary for dense retriever

--- wafl/retriever/dense_retriever.py
+++ wafl/retriever/dense_retriever.py
@@ -11,13 +11,21 @@ from wafl.retriever.base_retriever import BaseRetriever
 _path = os.path.dirname(__file__)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+_sentence_transfomers_dict = {
+    "msmarco-distilbert-base-v3": SentenceTransformer(
+        "msmarco-distilbert-base-v3", device=device
+    ),
+    "multi-qa-distilbert-dot-v1": SentenceTransformer(
+        "multi-qa-distilbert-dot-v1", device=device
+    ),
+}
 
 
 class DenseRetriever(BaseRetriever):
     _threshold_length = 5
 
     def __init__(self, model_name):
-        self._sentence_model = SentenceTransformer(model_name, device=device)
+        self._sentence_model = _sentence_transfomers_dict[model_name]
         self._embeddings_model = KeyedVectors(768)
 
     def add_text_and_index(self, text: str, index: str):
