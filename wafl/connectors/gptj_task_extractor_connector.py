43c3975328d7971799b96f9933dd1dbfcc9289ad
Fractalego <albertocetoli@gmail.com>
2023-04-10 17:51:27 +0100

    tying a different format for the task extractor prompt

--- wafl/connectors/gptj_task_extractor_connector.py
+++ wafl/connectors/gptj_task_extractor_connector.py
@@ -9,141 +9,141 @@ class GPTJTaskExtractorConnector(BaseGPTJConnector):
         prompt = (
             f"""
 ### Instruction:
-Extract the user's intention from the conversation. 
-If there is more than one task, separate them with "AND".
-### Input:
+Extract the user's intention from the conversation.
+<conversation>
 user: I want to drive the car
+</conversation>
 ### Response:
 the user wants to drive the car 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 bot: hello, how can I help
 user: Hello
+</conversation>
 ### Response:
 [statement] the user says hi 
 
 ### Instruction:
-Extract the user's intention from the conversation
-### Input:
+Extract the user's intention from the conversation.
+<conversation>
 user: goodbye
+</conversation>
 ### Response:
 [statement] the user says goodbye
  
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: hello
 user: the user's name is john
+</conversation>
 ### Response:
 [statement] the user says their name is John
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: my dog's name is Fido
+</conversation>
 ### Response:
 [statement] the user says their dog's name is Fido
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: thank you
+</conversation>
 ### Response:
 the user thanks 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: I want to drive a car
 bot: do you have a driving license?
 user: no
+</conversation>
 ### Response:
 the user wants to drive a car 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: I want to drive the car to London
+</conversation>
 ### Response:
 the user wants to drive the car to London 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: Can I have french fries
+</conversation>
 ### Response:
 the user asks if they can have french fries 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: Add oranges to the shopping list
+</conversation>
 ### Response:
 the user wants to add oranges to the shopping list 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: Hello
+</conversation>
 ### Response:
 the user greets
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 bot: what is your name
 user: my name is Alberto
+</conversation>
 ### Response:
 [statement] the user says their name is Alberto
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: remember that I am an engineer
+</conversation>
 ### Response:
 the user wants the bot to remember that they are an engineer
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: find the user a good restaurant and order a pizza
+</conversation>
 ### Response:
 [list of actions] the user wants this bot to find a good restaurant AND the user wants this bot to order a pizza
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: what is the time and how long is it before 12
+</conversation>
 ### Response:
 [list of actions] the user wants to know the time AND the user to know how long it is before 12 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: what musing is playing
+</conversation>
 ### Response:
 the user wants to know which music is playing 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: what is the the weather like
 user: what is the time
 ### Response:
@@ -151,27 +151,26 @@ the user wants to know the time
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: add paper
 user: add scissors
 user: add stone
-### Response:
+</conversation>
 the user wants to add stone 
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 user: what is the weather what is the temperature
+</conversation>
 ### Response:
 [list of actions] the user wants to know the weather AND the user wants to know the temperature
 
 ### Instruction:
 Extract the user's intention from the conversation.
-If there is more than one task, separate them with "AND".
-### Input:
+<conversation>
 {dialogue}
+</conversation>
 ### Response: 
         """.strip()
             + "\n"
