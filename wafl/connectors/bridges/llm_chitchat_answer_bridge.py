1c8cce8de4fcb14e5037676cbd52a0977d319408
Alberto Cetoli <albertocetoli@gmail.com>
2023-10-25 17:20:37 +0100

    tinkering with rules

--- wafl/connectors/bridges/llm_chitchat_answer_bridge.py
+++ wafl/connectors/bridges/llm_chitchat_answer_bridge.py
@@ -21,12 +21,25 @@ class LLMChitChatAnswerBridge:
     async def _get_answer_prompt(self, text, query, dialogue=None):
         prompt = f"""
 The following is a summary of a conversation. All the elements of the conversation are described briefly:
-{text}                     
-                                                                                                         
+{text}
+Rules:                     
+a) If the following applies: "The user asks you run a code" then follow all the steps below from top to bottom. 
+    Do *not* skip any step. Each step is a new interaction with the user. Do not complete more than one step at a time.
+    Step 1: If the code does not exist write it out.
+    Output the dialogue above and then write <step>1a</step> on a new line.
+    
+    Step 2: If the code already exists in the conversation ask the user if they want to change something in the code.
+    Output the dialogue above and then write <step>2a</step> on a new line.
+
+    Step 3: If the user still wants to go ahead, run the code locally as below
+      - Wrap it in the xml tag <execute> ... </execute>. Do *not* indent. Do *not* any other tag except <execute>.
+    Output the dialogue above and then write <step>3a</step> on a new line.
+
 Create a plausible dialogue based on the aforementioned summary. 
 Do not repeat yourself. Be friendly but not too servile.
-Wrap any code you output in the with the markdown syntax for code blocks (i.e. use triple backticks ```).
-    
+Wrap any code or html you output in the with the markdown syntax for code blocks (i.e. use triple backticks ```).
+  
+This is the dialogue:
 {dialogue}
 bot:
         """.strip()
