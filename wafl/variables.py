556ac1455cbe940a0a5e215faddfe0f21e936fb9
Alberto Cetoli <albertocetoli@gmail.com>
2024-05-11 12:16:40 +0100

    changed version

--- wafl/variables.py
+++ wafl/variables.py
@@ -1,9 +1,9 @@
 def get_variables():
     return {
-        "version": "0.0.82",
+        "version": "0.0.83",
     }
 
 
 def is_supported(wafl_llm_version):
-    supported_versions = ["0.0.82"]
+    supported_versions = ["0.0.82", "0.0.83"]
     return wafl_llm_version in supported_versions
