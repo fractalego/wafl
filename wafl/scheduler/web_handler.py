4a306f0f464e37007d899a27035fd793dc9b9f2d
Alberto Cetoli <albertocetoli@gmail.com>
2024-07-05 18:24:28 +0100

    added infobox

--- wafl/scheduler/web_handler.py
+++ wafl/scheduler/web_handler.py
@@ -103,13 +103,27 @@ class WebHandler:
 
     async def get_info(self):
         info = await self._information_client.get_information()
+        is_clicked = request.form.get("clicked")
+        is_clicked = "false" if is_clicked == "true" else "true"
+        infobox = ""
+        if is_clicked == "true":
+            infobox = f"""
+            <div class="bg-white shadow-lg rounded-lg p-4 absolute w-max left-20">
+                <div style="color:black;font-size:12px;"><b>Model name:</b> {info['model_name']}</div>
+                <div style="color:black;font-size:12px;"><b>Backend version:</b> {info['backend_version']}</div>
+            </div>
+            """
         return f"""
-        <div id="info"
-              hx-post="/{self._conversation_id}/get_info"
-                hx-swap="innerHTML"
-                hx-target="#info"
-                hx-trigger="load"
-        >{info}</div>
+        <a title="Info"
+           hx-post="/{self._conversation_id}/get_info"
+           hx-vals='{{"clicked": "{is_clicked}"}}'
+           hx-swap="outerHTML"
+           class="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group">
+           <svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" class="w-6 h-6" viewBox="0 0 50 50">
+           <path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 25 11 A 3 3 0 0 0 22 14 A 3 3 0 0 0 25 17 A 3 3 0 0 0 28 14 A 3 3 0 0 0 25 11 z M 21 21 L 21 23 L 22 23 L 23 23 L 23 36 L 22 36 L 21 36 L 21 38 L 22 38 L 23 38 L 27 38 L 28 38 L 29 38 L 29 36 L 28 36 L 27 36 L 27 21 L 26 21 L 22 21 L 21 21 z"></path>
+           </svg>
+            {infobox}
+        </a>
         """
 
     async def run(self):
